f3e6f25be394f59b614675a96e233dd98979c171
wrongwrong <boranti1995@gmail.com>
2021-03-20 17:11:02 +0900

    必要な依存を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,12 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     implementation("org.springframework.boot:spring-boot-starter-json")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
