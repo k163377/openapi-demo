bf9eec55ffb74915d479a0777fabda5ef2d1bf06
wrongwrong <boranti1995@gmail.com>
2021-03-20 14:44:19 +0900

    init

---
+++ src/main/kotlin/com/wrongwrong/openapidemo/OpenapiDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.wrongwrong.openapidemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenapiDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<OpenapiDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/wrongwrong/openapidemo/OpenapiDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wrongwrong.openapidemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenapiDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
