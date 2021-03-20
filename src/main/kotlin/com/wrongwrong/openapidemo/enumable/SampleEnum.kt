35b3d9c733b700bae6b5cdbd8c561dd2fb94ac84
wrongwrong <boranti1995@gmail.com>
2021-03-20 17:49:28 +0900

    Swaggerに反映されるよう修正

--- src/main/kotlin/com/wrongwrong/openapidemo/enumable/SampleEnum.kt
+++ src/main/kotlin/com/wrongwrong/openapidemo/enumable/SampleEnum.kt
@@ -5,6 +5,6 @@ import com.fasterxml.jackson.annotation.JsonValue
 enum class SampleEnum(label: String) {
     `enum`("label of enum"), `class`("label of class");
 
-    @JsonValue
-    private val jsonValue = mapOf("name" to name, "label" to label)
+    @get:JsonValue
+    val jsonValue = mapOf("name" to name, "label" to label)
 }
