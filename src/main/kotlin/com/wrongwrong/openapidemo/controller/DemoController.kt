ddfffa4dcb5b2580a7fdbe9eddac353a6d999cde
wrongwrong <boranti1995@gmail.com>
2021-03-21 22:01:41 +0900

    secured、JavaDoc共に反応なし

--- src/main/kotlin/com/wrongwrong/openapidemo/controller/DemoController.kt
+++ src/main/kotlin/com/wrongwrong/openapidemo/controller/DemoController.kt
@@ -4,10 +4,15 @@ import com.wrongwrong.openapidemo.enumable.SampleEnum
 import com.wrongwrong.openapidemo.form.SampleForm
 import com.wrongwrong.openapidemo.view.SampleView
 import io.swagger.annotations.ApiParam
+import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class DemoController {
+    /**
+     * @return パラメータに設定したEnumに対応するView
+     */
+    @Secured(value = [ "ROLE_SAMPLE" ])
     @GetMapping("/sample/get")
     fun getSample(@RequestParam @ApiParam(allowableValues = "Hoge,Fuga") enum: SampleEnum): SampleView {
         return SampleView(enum)
